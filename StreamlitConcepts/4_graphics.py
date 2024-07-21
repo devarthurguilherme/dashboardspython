fc9694d0132678aa0b7242bd62e511309cbdd249
devarthurguilherme <arthurguilherme2013@gmail.com>
2024-07-21 12:36:29 +0100

    Studies about Graphics: Altair, Graphviz, PyDeck.

--- StreamlitConcepts/4_graphics.py
+++ StreamlitConcepts/4_graphics.py
@@ -4,6 +4,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import altair as alt
+import plotly.figure_factory as ff
+import graphviz
+import pydeck as pdk
 
 
 # Line Chart
@@ -45,3 +49,95 @@ fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
+
+# Altair
+st.header("Altair")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+c = (
+    alt.Chart(chart_data)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.altair_chart(c, use_container_width=True)
+
+
+# Plotly
+st.header("Plotly")
+
+# Add histogram data
+# x1 = np.random.randn(200) - 2
+# x2 = np.random.randn(200)
+# x3 = np.random.randn(200) + 2
+
+# # Group data together
+# hist_data = [x1, x2, x3]
+
+# group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# # Create distplot with custom bin_size
+# fig = ff.create_distplot(
+#     hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# # Plot!
+# st.plotly_chart(fig, use_container_width=True)
+
+
+# Graphviz
+st.header("Graphviz")
+
+# Create a graphlib graph object
+graph = graphviz.Digraph()
+graph.edge('run', 'intr')
+graph.edge('intr', 'runbl')
+graph.edge('runbl', 'run')
+graph.edge('run', 'kernel')
+graph.edge('kernel', 'zombie')
+graph.edge('kernel', 'sleep')
+graph.edge('kernel', 'runmem')
+graph.edge('sleep', 'swap')
+graph.edge('swap', 'runswap')
+graph.edge('runswap', 'new')
+graph.edge('runswap', 'runmem')
+graph.edge('new', 'runmem')
+graph.edge('sleep', 'runmem')
+
+st.graphviz_chart(graph)
+
+
+# PyDeck
+st.header("Graphviz")
+
+chart_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+            'HexagonLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
